06882b0f61905f4ad27cf6f2704830aab3b81a0a
József Farkas <fjozsef001@gmail.com>
2016-02-01 20:25:39 +0100

    Solution for problem: CPTTRN2 - Character Patterns (Act 2)

---
+++ CPTTRN2/cpttrn2.cc
@@ -0,0 +1,26 @@
+// Author: József Farkas
+// Github: https://github.com/fjozsef
+
+#include <cstdio>
+
+int main() {
+	int num_test_cases;
+	int num_lines, num_columns;
+
+	scanf("%d", &num_test_cases);
+
+	while (num_test_cases--) {
+		scanf("%d %d", &num_lines, &num_columns);
+		for (int i = 0; i < num_lines; ++i) {
+			for (int j = 0; j < num_columns; ++j) {
+				putchar( (i == 0 || j == 0 || i == num_lines - 1 || j == num_columns - 1) ? '*' : '.');
+			}
+			putchar('\n');
+		}
+		if (num_test_cases > 0) {
+			putchar('\n');
+		}
+	}
+
+	return 0;
+}
