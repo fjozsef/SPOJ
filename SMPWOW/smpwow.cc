f1657f6a4eb93785c6110e5181b6f38fc0626ad3
József Farkas <fjozsef001@gmail.com>
2016-02-06 18:56:26 +0100

    Solution for problem: SMPWOW - Wow

---
+++ SMPWOW/smpwow.cc
@@ -0,0 +1,16 @@
+// Author: József Farkas
+// Github: https://github.com/fjozsef
+
+#include <cstdio>
+
+int main() {
+	int times;
+	scanf("%d", &times);
+	putchar('W');
+	while(times--){
+		putchar('o');
+	}
+	putchar('w');
+	putchar('\n');
+	return 0;
+}
